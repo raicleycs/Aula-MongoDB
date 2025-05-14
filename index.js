d6ee237a9b27248f3df7094284aec8e6366dd064
Raicley Contadini Sampaio <raicleysamp@gmail.com>
2025-05-14 19:50:29 -0300

    Update arquivos projeto

---
+++ index.js
@@ -0,0 +1,29 @@
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://raicleysamp:Informatica01.@clusterappmongodb.bhzsmnb.mongodb.net/?retryWrites=true&w=majority&appName=ClusterAppMongoDB";
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+const dbName = 'dado';
+
+async function run() {
+    try{
+        await client.connect();
+        const db = client.db(dbName);
+        const collection = db.collection('DadosUsuarios');
+        console.log("Buscando Dados");
+
+        const todosUsuarios = await collection.find({}).toArray();
+        console.log("Usuarios encontrados:", JSON.stringify(todosUsuarios, null,2));
+    }catch(erro){
+        console.error('Erro encontrado', erro);
+    }finally{
+        await client.close();
+    }
+}
+run().catch(console.dir);
